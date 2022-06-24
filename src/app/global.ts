1b1cbff75c6dbc193d6b37e37be5a7d74ef8935e
VINCENT CHONLONG MAURO RENATO <mvincent@pichincha.com>
2022-06-24 12:30:09 -0500

    Primera subida

---
+++ src/app/global.ts
@@ -0,0 +1,7 @@
+//
+// ===== File globals.ts    
+//
+'use strict';
+
+export const urlApi='https://bp-todolist.herokuapp.com';
+export const idAuthor = 21;
--- src/app/todo-list/components/add-todo/add-todo.component.ts
+++ src/app/todo-list/components/add-todo/add-todo.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '../../interfaces/todo';
+import { TodoService } from '../../services/todo.service';
+import { StateService } from '../../services/state.service';
+import {TodoWrapperComponent} from '../todo-wrapper/todo-wrapper.component';
 
 @Component({
   selector: 'app-add-todo',
@@ -9,9 +16,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddTodoComponent implements OnInit {
 
   public frmTodo: FormGroup;
+  todo: Todo;
 
   constructor(
-    private readonly fb: FormBuilder) {
+    private http: HttpClient,
+    private todoService: TodoService,
+    private stateService: StateService,
+    private readonly fb: FormBuilder,
+    private router: Router) {
   }
 
   ngOnInit(): void {
@@ -22,8 +34,27 @@ export class AddTodoComponent implements OnInit {
   }
 
   onClickAdd() {
+    if (this.frmTodo.valid) {
+      this.todo = {
+        description: this.frmTodo.get('descriptionTodo')?.value,
+        finish_at: this.frmTodo.get('finishAt')?.value,
+        created_at: new Date(Date.now()).toISOString(),
+        id_author: 0,
+        status: 1
+      };
 
+
+      this.todoService.addTodo(this.todo).subscribe(dato => {
+        swal.fire('Registro exitoso...', "", 'success');
+      }, error => console.log(error));
+      //this.todoWrapper.getListTodo();
+      //this.router.navigate(['/']);
+    }
   }
+  onClickVolver() {
+    this.router.navigate(['/']);
+  }
+
 
 
 }
